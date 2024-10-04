075b3ef9fde482dd04b43022d2cbacfabfeae9c9
guramius <96048349+guramius@users.noreply.github.com>
2024-10-04 20:14:35 +0400

    make responvsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': { 'max': '740px' },
+      },
+      colors: {
+        'textColor': '#CAC9FF',
+      },
+      backgroundImage: {
+        'gradientCustom': 'linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%), #F55', 
+      },
+    }  
+  },
+  variants: {
+    extend: {
+      display: ['mobile'], 
+    }
   },
   plugins: [],
 }
